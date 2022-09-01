34f84d5b3d884bc5a9aac426a630b6cd139b9d86
AnatolSalau <salovanatoly5@mail.ru>
2022-09-01 23:54:14 +0300

    Throw messages from another class

---
+++ src/main/resources/table_with_search/table_one.js
@@ -0,0 +1,7 @@
+$('.table td').resizable({
+    resize: function(event,ui) {
+        var col = $(this).index()+1;
+        $(this).closest('table').find('tr td:nth-child('+col+')').width(ui.size.width);
+        $(this).parent().find('td').height(ui.size.height);
+    }
+});
