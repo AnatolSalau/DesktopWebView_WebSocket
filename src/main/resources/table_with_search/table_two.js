6408af171717b8bd1c1957b4d49507f8d5c405cf
AnatolSalau <salovanatoly5@mail.ru>
2022-09-01 16:25:33 +0300

    Slider JS

--- src/main/resources/table_with_search/table_two.js
+++ src/main/resources/table_with_search/table_two.js
@@ -1,12 +1,37 @@
-$(document).ready(function() {
-/*    $('[data-toggle="tooltip"]').tooltip();*/
-    $('table.resizable').resizableColumns();
-})
+$(function() {
 
-function toggleProfile() {
-    if ($('#profile').height() == 0) {
-        $('#profile').height(285);
-    } else {
-        $('#profile').height(0);
-    }
-}
+    $('.narrow-table').tablesorter({
+        theme : 'blue',
+        // initialize zebra striping and resizable widgets on the table
+        widgets: [ 'zebra', 'resizable', 'stickyHeaders' ],
+        widgetOptions: {
+            // storage_useSessionStorage : true, deprecated in v2.28.8
+            storage_storageType: 's', // use first letter (s)ession
+            resizable_addLastColumn : true
+        }
+    });
+
+    // overflow table
+    $('.wrapper table').tablesorter({
+        theme: 'blue',
+        widgets: ['zebra', 'resizable'],
+        widgetOptions: {
+            resizable_addLastColumn : true,
+            resizable_widths : [ '100px', '60px', '30px', '50px', '60px', '140px' ]
+        }
+    });
+
+    $('.full-width-table').tablesorter({
+        theme : 'blue',
+        // initialize zebra striping and resizable widgets on the table
+        widgets: [ 'zebra', 'resizable', 'stickyHeaders' ],
+        widgetOptions: {
+            resizable: true,
+            // These are the default column widths which are used when the table is
+            // initialized or resizing is reset; note that the "Age" column is not
+            // resizable, but the width can still be set to 40px here
+            resizable_widths : [ '10%', '10%', '40px', '10%', '100px' ]
+        }
+    });
+
+});
