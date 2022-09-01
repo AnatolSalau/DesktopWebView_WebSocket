efd0b978730c3d12660a48dbf6227303847205e5
AnatolSalau <salovanatoly5@mail.ru>
2022-09-01 15:34:24 +0300

    Slider JS

--- src/main/resources/table_with_search/table_two.js
+++ src/main/resources/table_with_search/table_two.js
@@ -1,17 +1,12 @@
-$.ripple("#myTab1", {
-    debug: false, // Turn Ripple.js logging on/off
-    on: 'mousedown', // The event to trigger a ripple effect
+$(document).ready(function() {
+/*    $('[data-toggle="tooltip"]').tooltip();*/
+    $('table.resizable').resizableColumns();
+})
 
-    opacity: 1, // The opacity of the ripple
-    color: "hsla(291, 73%, 94%, 0.3)", // Set the background color. If set to "auto", it will use the text color
-    multi: false, // Allow multiple ripples per element
-
-    duration: 0.5, // The duration of the ripple
-
-    // Filter function for modifying the speed of the ripple
-    rate: function(pxPerSecond) {
-        return pxPerSecond;
-    },
-
-    easing: 'linear' // The CSS3 easing function of the ripple
-});
+function toggleProfile() {
+    if ($('#profile').height() == 0) {
+        $('#profile').height(285);
+    } else {
+        $('#profile').height(0);
+    }
+}
